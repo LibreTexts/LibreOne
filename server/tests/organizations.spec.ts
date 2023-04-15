4542c60956ad1ce60168d0f550f61325a6741384
Ethan Turner <ethanaturner13@gmail.com>
2023-04-14 19:46:09 -0700

    refactor: divide Organization tests by action type

--- server/tests/organizations.spec.ts
+++ server/tests/organizations.spec.ts
@@ -1,4 +1,4 @@
-import _ from 'lodash-es';
+import _ from 'lodash';
 import { after, afterEach, describe, it } from 'mocha';
 import { expect } from 'chai';
 import request from 'supertest';
@@ -23,32 +23,34 @@ describe('Organizations', async () => {
     await Organization.destroy({ where: {} });
   });
 
-  it('should get organization', async () => {
-    const org = await Organization.create({ name: 'LibreTexts' });
-    const response = await request(server).get(`/api/v1/organizations/${org.id}`);
-    expect(response.status).to.equal(200);
-    expect(_.omit(response.body?.data, omitFields)).to.deep.equal({
-      ..._.omit(org.get(), omitFields),
-      ...defaultFields(),
-      system_id: null,
+  describe('READ', () => {
+    it('should get organization', async () => {
+      const org = await Organization.create({ name: 'LibreTexts' });
+      const response = await request(server).get(`/api/v1/organizations/${org.id}`);
+      expect(response.status).to.equal(200);
+      expect(_.omit(response.body?.data, omitFields)).to.deep.equal({
+        ..._.omit(org.get(), omitFields),
+        ...defaultFields(),
+        system_id: null,
+      });
     });
-  });
-  it('should get all organizations', async () => {
-    const [org1, org2] = await Organization.bulkCreate([
-      { name: 'Org1' },
-      { name: 'Org2' },
-    ]);
-    const response = await request(server).get('/api/v1/organizations');
-    expect(response.status).to.equal(200);
-    expect(response.body?.data).to.have.length(2);
-    const [out1, out2] = response.body.data;
-    expect(_.omit(out1, omitFields)).to.deep.equal({
-      ..._.omit(org1.get(), omitFields),
-      ...defaultFields(),
-    });
-    expect(_.omit(out2, omitFields)).to.deep.equal({
-      ..._.omit(org2.get(), omitFields),
-      ...defaultFields(),
+    it('should get all organizations', async () => {
+      const [org1, org2] = await Organization.bulkCreate([
+        { name: 'Org1' },
+        { name: 'Org2' },
+      ]);
+      const response = await request(server).get('/api/v1/organizations');
+      expect(response.status).to.equal(200);
+      expect(response.body?.data).to.have.length(2);
+      const [out1, out2] = response.body.data;
+      expect(_.omit(out1, omitFields)).to.deep.equal({
+        ..._.omit(org1.get(), omitFields),
+        ...defaultFields(),
+      });
+      expect(_.omit(out2, omitFields)).to.deep.equal({
+        ..._.omit(org2.get(), omitFields),
+        ...defaultFields(),
+      });
     });
   });
 });
