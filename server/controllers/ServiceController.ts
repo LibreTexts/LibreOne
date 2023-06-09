b829e6f2a6003c8416a56c1cd66a498e1de29b71
Ethan Turner <ethanaturner13@gmail.com>
2023-06-08 20:02:38 -0700

    refactor(ServiceController): convert to class

--- server/controllers/ServiceController.ts
+++ server/controllers/ServiceController.ts
@@ -3,107 +3,109 @@ import { Service, sequelize } from '../models';
 import { getProductionURL, safeJSONParse } from '../helpers';
 import errors from '../errors';
 
-/**
- * Creates a new Service definition.
- *
- * @param req - Incoming API request.
- * @param res - Outgoing API response.
- * @returns The fulfilled API response.
- */
-export async function createService(req: Request, res: Response): Promise<Response> {
-  const parsedServiceBody = safeJSONParse(req.body.body);
-  if (!parsedServiceBody) {
-    return errors.badRequest(res);
-  }
-  const newService = await Service.create(req.body);
-  const resourceURL = `${getProductionURL()}/api/v1/services/${newService.get('id')}`;
-  return res.status(201).set('Location', resourceURL).send({
-    data: newService,
-  });
-}
-
-/**
- * Retrieves a single Service definition.
- *
- * @param req - Incoming API request.
- * @param res - Outgoing API response.
- * @returns The fulfilled API response.
- */
-export async function getService(req: Request, res: Response): Promise<Response> {
-  const { id } = req.params;
-  const serviceID = Number(id);
-  const foundService = await Service.findByPk(serviceID);
-  if (!foundService) {
-    return errors.notFound(res);
+export class ServiceController {
+  /**
+   * Creates a new Service definition.
+   *
+   * @param req - Incoming API request.
+   * @param res - Outgoing API response.
+   * @returns The fulfilled API response.
+   */
+  public async createService(req: Request, res: Response): Promise<Response> {
+    const parsedServiceBody = safeJSONParse(req.body.body);
+    if (!parsedServiceBody) {
+      return errors.badRequest(res);
+    }
+    const newService = await Service.create(req.body);
+    const resourceURL = `${getProductionURL()}/api/v1/services/${newService.get('id')}`;
+    return res.status(201).set('Location', resourceURL).send({
+      data: newService,
+    });
   }
 
-  return res.send({
-    data: foundService,
-  });
-}
+  /**
+   * Retrieves a single Service definition.
+   *
+   * @param req - Incoming API request.
+   * @param res - Outgoing API response.
+   * @returns The fulfilled API response.
+   */
+  public async getService(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const serviceID = Number(id);
+    const foundService = await Service.findByPk(serviceID);
+    if (!foundService) {
+      return errors.notFound(res);
+    }
 
-/**
- * Retrieves all known Service definitions.
- *
- * @param req - Incoming API request.
- * @param res - Outgoing API response.
- * @returns The fulfilled API response.
- */
-export async function getAllServices(req: Request, res: Response): Promise<Response> {
-  const offset = Number(req.query.offset);
-  const limit = Number(req.query.limit);
-  const { count, rows } = await Service.findAndCountAll({
-    offset,
-    limit,
-    order: sequelize.col('name'),
-    attributes: ['id', 'name', 'service_Id'],
-  });
-  return res.send({
-    meta: {
+    return res.send({
+      data: foundService,
+    });
+  }
+
+  /**
+   * Retrieves all known Service definitions.
+   *
+   * @param req - Incoming API request.
+   * @param res - Outgoing API response.
+   * @returns The fulfilled API response.
+   */
+  public async getAllServices(req: Request, res: Response): Promise<Response> {
+    const offset = Number(req.query.offset);
+    const limit = Number(req.query.limit);
+    const { count, rows } = await Service.findAndCountAll({
       offset,
       limit,
-      total: count,
-    },
-    data: rows,
-  });
-}
+      order: sequelize.col('name'),
+      attributes: ['id', 'name', 'service_Id'],
+    });
+    return res.send({
+      meta: {
+        offset,
+        limit,
+        total: count,
+      },
+      data: rows,
+    });
+  }
 
-/**
- * Updates a Service definition.
- *
- * @param req - Incoming API request.
- * @param res - Outgoing API response.
- * @returns The fulfilled API response.
- */
-export async function updateService(req: Request, res: Response): Promise<Response> {
-  const { id } = req.params;
-  const serviceID = Number(id);
-  const parsedServiceBody = safeJSONParse(req.body.body);
-  if (!parsedServiceBody) {
-    return errors.badRequest(res);
+  /**
+   * Updates a Service definition.
+   *
+   * @param req - Incoming API request.
+   * @param res - Outgoing API response.
+   * @returns The fulfilled API response.
+   */
+  public async updateService(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const serviceID = Number(id);
+    const parsedServiceBody = safeJSONParse(req.body.body);
+    if (!parsedServiceBody) {
+      return errors.badRequest(res);
+    }
+    const [numUpdated] = await Service.update(req.body, {
+      where: { id: serviceID },
+    });
+    if (numUpdated !== 1) {
+      return errors.internalServerError(res);
+    }
+    return res.status(200);
   }
-  const [numUpdated] = await Service.update(req.body, {
-    where: { id: serviceID },
-  });
-  if (numUpdated !== 1) {
-    return errors.internalServerError(res);
-  }
-  return res.status(200);
-}
 
-/**
- * Deletes a Service definition.
- *
- * @param req - Incoming API request.
- * @param res - Outgoing API response.
- * @returns The fulfilled API response.
- */
-export async function deleteService(req: Request, res: Response): Promise<Response> {
-  const { id } = req.params;
-  const serviceID = Number(id);
-  const numDeleted = await Service.destroy({ where: { id: serviceID }});
-  if (numDeleted < 1) {
-    return errors.internalServerError(res);
+  /**
+   * Deletes a Service definition.
+   *
+   * @param req - Incoming API request.
+   * @param res - Outgoing API response.
+   * @returns The fulfilled API response.
+   */
+  public async deleteService(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const serviceID = Number(id);
+    const numDeleted = await Service.destroy({ where: { id: serviceID }});
+    if (numDeleted < 1) {
+      return errors.internalServerError(res);
+    }
+    return res.status(200);
   }
-  return res.status(200);
 }
--- server/routes/services.ts
+++ server/routes/services.ts
@@ -1,20 +1,22 @@
 import express from 'express';
 import { catchInternal } from '../helpers';
-import * as ServiceController from '../controllers/ServiceController';
+import { ServiceController } from '../controllers/ServiceController';
 import { ensureActorIsAPIUser, verifyAPIAuthentication } from '../middleware';
 
 const servicesRouter = express.Router();
+const controller = new ServiceController();
+
 servicesRouter.use(verifyAPIAuthentication);
 servicesRouter.use(ensureActorIsAPIUser);
 
 servicesRouter.route('/')
-  .get(catchInternal(ServiceController.getAllServices))
-  .post(catchInternal(ServiceController.createService));
+  .get(catchInternal((req, res) => controller.getAllServices(req, res)))
+  .post(catchInternal((req, res) => controller.createService(req, res)));
 
 servicesRouter.route('/:serviceID')
-  .get(catchInternal(ServiceController.getService))
-  .put(catchInternal(ServiceController.updateService))
-  .delete(catchInternal(ServiceController.deleteService));
+  .get(catchInternal((req, res) => controller.getService(req, res)))
+  .put(catchInternal((req, res) => controller.updateService(req, res)))
+  .delete(catchInternal((req, res) => controller.deleteService(req, res)));
 
 export {
   servicesRouter,
