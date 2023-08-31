ddeae67af26a5362340005585d5f8c016f9d669c
Jake Turner <52841588+jakeaturner@users.noreply.github.com>
2023-08-30 21:44:39 -0700

    feat(IDM): manage avatar (#18)

---
+++ components/account_management/UserAvatar.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    :src="src"
+    :width="width"
+    class="avatar-img"
+  >
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  const DEFAULT_AVATAR_URL =
+    'https://cdn.libretexts.net/DefaultImages/avatar.png';
+  const props = withDefaults(
+    defineProps<{
+      src: string;
+      width: number;
+    }>(),
+    {
+      src: '',
+      width: 25,
+    },
+  );
+
+  const src = computed(() => {
+    if (props.src) {
+      return props.src;
+    
+    }
+    return DEFAULT_AVATAR_URL;
+  });
+</script>
+
+<style>
+.avatar-img {
+  border-radius: 50%;
+  border: 0.5px solid #e5e7eb;
+}
+</style>
