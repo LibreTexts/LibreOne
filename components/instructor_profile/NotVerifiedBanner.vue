e2bc3ae595c7625aebeea30332d41f79186833f7
Jake Turner <turnerj804@gmail.com>
2023-10-23 19:12:51 -0700

    feat(Launchpad): add instructor verification reminder banner

---
+++ components/instructor_profile/NotVerifiedBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="p-4 mb-4 text-sm text-white rounded-lg bg-blue-600"
+    role="alert"
+  >
+    <p class="font-medium text-white">
+      {{ $t("instructor.verify_reminder_header") }}
+    </p>
+    <i18n-t
+      keypath="instructor.verify_reminder_msg"
+      tag="p"
+    >
+      <template #instructorVerificationLink>
+        <a
+          href="/instructor"
+          target="_blank"
+          rel="noreferer"
+          class="text-yellow-400 hover:text-yellow-100"
+        >
+          {{ $t("common.here") }}
+        </a>
+      </template>
+    </i18n-t>
+  </div>
+</template>
