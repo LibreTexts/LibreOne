9293b4cb4dba3f7ac7743d9d02fe5df8f6a689f1
Jake Turner <52841588+jakeaturner@users.noreply.github.com>
2024-02-24 10:09:18 -0800

    feat: add Support Center widget (#61)

--- components/BaseLayout.vue
+++ components/BaseLayout.vue
@@ -1,6 +1,16 @@
 <template>
   <slot />
+  <div id="support-widget-container" class="support-center-widget"></div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const supportScript = document.createElement("script");
+  supportScript.src =
+    "https://cdn.libretexts.net/libretexts-support-widget.min.js";
+  supportScript.async = true;
+  document.body.appendChild(supportScript);
+});
 </script>
