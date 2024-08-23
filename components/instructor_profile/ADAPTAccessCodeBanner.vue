abe23f6b1a43fb92008ab20b50696167a3dd8084
Jake Turner <jturner@cosmistack.com>
2024-08-23 13:55:19 -0700

    feat(Instructors): add ADAPT access guidance

---
+++ components/instructor_profile/ADAPTAccessCodeBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="p-4 mb-4 text-sm text-white rounded-lg bg-blue-600"
+    role="alert"
+  >
+    <p class="font-medium text-white">
+      {{ $t("instructor.adapt_access_header") }}
+    </p>
+    <p>
+      {{ $t("instructor.adapt_access_msg") }}
+      {{ ' ' }}
+      <a
+        href="https://commons.libretexts.org/support/contact?category=adaptcode&priority=medium"
+        target="_blank"
+        rel="noreferer"
+        class="hover:text-amber-100 underline"
+      >
+        {{ $t("instructor.adapt_access_link") }}
+      </a>
+    </p>
+  </div>
+</template>
