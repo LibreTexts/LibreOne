f4bd65615fe0ac0b08343b391586cc920fea7992
Akhileshwar Shriram <112577383+AkhilTheBoss@users.noreply.github.com>
2026-06-29 12:36:45 -0700

    feat(UI): switch to Davis Vue UI (#262)
    * refactor: changed from semantic-ui to davis-vue
    * chore(deps): fix broken package.json and regen lockfile
    ---------
    Co-authored-by: jakeaturner <jturner@cosmistack.com>

--- components/complete_registration/RoleForm.vue
+++ components/complete_registration/RoleForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div aria-live="polite" :aria-busy="loading">
-    <h1 class="text-center text-3xl font-medium">
+    <Heading :level="3" class="text-center">
       {{
         $t("complete_registration_role.header", {
           name: pageContext?.user?.first_name,
         })
       }}
-    </h1>
+      </Heading>
     <p class="text-center mt-4">
       {{ $t("complete_registration_role.tagline") }}
     </p>
@@ -23,24 +23,46 @@
         enter-active-class="motion-safe:transition-transform motion-safe:ease-out motion-safe:duration-500"
         leave-active-class="motion-safe:transition-transform motion-safe:ease-in motion-safe:duration-300"
       >
-        <div class="mt-8">
-          <button
-            class="inline-flex items-center justify-center h-10 bg-primary p-2 rounded-md text-white w-full font-medium hover:bg-sky-700 hover:shadow"
-            @click="submitRoleUpdate('student')"
-          >
-            {{ $t("complete_registration_role.student_user") }}
-          </button>
-          <button
-            class="inline-flex items-center justify-center h-10 bg-primary p-2 mt-4 rounded-md text-white w-full font-medium hover:bg-sky-700 hover:shadow"
-            @click="submitRoleUpdate('instructor')"
-          >
-            {{ $t("complete_registration_role.instructor_user") }}
-          </button>
-          <p class="text-center text-gray-500 text-xs mt-3 mx-3">
-            <FontAwesomeIcon icon="fa-solid fa-circle-info" />
-            {{ $t("complete_registration_role.instructor_info") }}
-          </p>
-        </div>
+        <template v-if="!isInstructorType">
+          <div class="mt-8 flex flex-col gap-4">
+            <Button full-width @click="handleStudentClick">
+              {{ $t("complete_registration_role.student_user") }}
+            </Button>
+            <Button full-width @click="isInstructorType = true">
+              {{ $t("complete_registration_role.instructor_user") }}
+            </Button>
+            <p class="text-center text-gray-500 text-xs mt-3 mx-3">
+              <FontAwesomeIcon icon="fa-solid fa-circle-info" />
+              {{ $t("complete_registration_role.instructor_info") }}
+            </p>
+          </div>
+        </template>
+
+        <template v-else>
+          <div class="mt-8 flex flex-col gap-4">
+            <Button
+              v-for="(role, idx) in ADAPT_SPECIAL_ROLES"
+              :key="role"
+              full-width
+              @click="handleAdaptSpecialRoleClick(role)"
+            >
+              {{ $t(`complete_registration_role.adapt_special_roles.${role}`) }}
+            </Button>
+            <div class="flex items-center justify-center">
+              <a
+                @click="isInstructorType = false"
+                class="text-gray-500 hover:text-accent text-center mt-4 cursor-pointer"
+              >
+                <FontAwesomeIcon icon="fa-solid fa-circle-arrow-left" />
+                {{ $t("common.goback") }}
+              </a>
+            </div>
+            <p class="text-center text-gray-500 text-xs mt-3 mx-3">
+              <FontAwesomeIcon icon="fa-solid fa-circle-info" />
+              {{ $t("complete_registration_role.adapt_special_roles_desc") }}
+            </p>
+          </div>
+        </template>
       </Transition>
     </template>
     <div v-else class="flex items-center justify-center p-8">
@@ -55,6 +77,7 @@ import { useAxios } from "@renderer/useAxios";
 import { usePageContext } from "@renderer/usePageContext";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import LoadingIndicator from "@components/LoadingIndicator.vue";
+import { Button, Heading } from "@libretexts/davis-vue";
 
 const props = defineProps<{ uuid: string }>();
 const emit = defineEmits<{
@@ -73,9 +96,7 @@ const loading = ref(false);
 async function submitRoleUpdate(role: string) {
   loading.value = true;
   try {
-    await axios.patch(`/users/${props.uuid}`, {
-      user_type: role,
-    });
+    await axios.patch(`/users/${props.uuid}`, { user_type: role });
     loading.value = false;
     emit("role-update");
   } catch (e) {
